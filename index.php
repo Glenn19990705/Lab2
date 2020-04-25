04f5d394df083ff1c80ada726931137b46d3c375
Glenn19990705 <cristiaensglenn@outlook.be>
2020-04-25 15:30:05 +0200

    Index pagina + login pagina

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>IMDFlix</title>
+  </head>
+<body>
+  
+  <div id="netflix">
+  
+  
+
+  <div class="collection">
+
+       <a class="collection__item">
+    </a>
+    
+  </div>
+  
+</div>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="$_POST">
+      <input id="firstname" name="firstname" placeholder="firstname">
+      <input id="lastname" name="lastname" placeholder="lastname">
+      <input id="email" name="email" placeholder="email">
+      <input id="password" name="password" placeholder="password">
+
+
+  </form>
+</body>
+</html>
